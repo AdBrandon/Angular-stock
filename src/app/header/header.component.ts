f62cbe56ceb6dcc4032dbdceaed7fe412b73e26d
brandon <brandon6608772@gmail.com>
2017-08-07 10:15:01 +0800

    update

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -10,28 +10,28 @@ import {environment} from "../../environments/environment";
 })
 export class HeaderComponent implements OnInit {
 
-  messageCount = 0;
-  notifications = 0;
-  tasks = 0;
+  messageCount = 5;
+  notifications = 8;
+  tasks = 4;
 
   constructor(public router:Router,public messageServer:MessageService) { }
 
   ngOnInit() {
-    this.messageServer.createObservableSocket("ws://"+environment.address+":8085")
-      .map(event => JSON.parse(event))
-      .subscribe(
-        event => {
-          if (event.messageCount > 0){
-            this.messageCount = event.messageCount;
-          }
-          if (event.notifications > 0){
-            this.notifications = event.notifications;
-          }
-          if (event.tasks > 0){
-            this.tasks = event.tasks;
-          }
-        }
-      )
+    // this.messageServer.createObservableSocket("ws://"+environment.address+":8085")
+    //   .map(event => JSON.parse(event))
+    //   .subscribe(
+    //     event => {
+    //       if (event.messageCount > 0){
+    //         this.messageCount = event.messageCount;
+    //       }
+    //       if (event.notifications > 0){
+    //         this.notifications = event.notifications;
+    //       }
+    //       if (event.tasks > 0){
+    //         this.tasks = event.tasks;
+    //       }
+    //     }
+    //   )
   }
 
 }
